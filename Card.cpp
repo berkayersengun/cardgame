b552049f7c9adbd3724f21b99061183a991c0a72
Jansen Ostrovsky <shady2k12@users.noreply.github.com>
2017-11-16 03:39:14 +0000

    Set of cards created

--- Card.cpp
+++ Card.cpp
@@ -1,40 +1,118 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector>
 #include "Card.h"
-#include <string>
 
 using namespace std;
 // create a “blank” card
-Card::Card() : rank(0), suit(0)
-{
-
-}
+Card::Card() = default;
 
 // constructor to create a card, setting the suit and rank
-Card::Card(int s, int r)
-{
-	rank = r;
-	suit = s;
-}
+Card::Card(Rank rank, Suit suit) : rank(rank), suit(suit) {}
 
 // destructor to create a card, setting the suit and rank
-Card::~Card()
-{
-}
+Card::~Card() {}
 
 // set an existing card to a particular value
-void Card::setCard(int s, int r)
+void Card::setCard(Rank rank, Suit suit)
 {
-	rank = r;
-	suit = s;
+	rank = rank;
+	suit = suit;
 }
 
-// return the point value of the card. Ace = 1, 2 thru 10, face cards can be 11, 12, 
-int Card::getValue()
+// return rank two=2 through to Ace
+int Card::getRank()
+{
+	return rank;
+}
+// return suit SPA, HRT, CLB, DIA
+int Card::getSuit()
 {
 	return rank;
 }
 
-void Card::printCard()
+std::ostream& operator<<(std::ostream& os, const Card& c)
 {
-   cout << "Card is rank : " << suit << " and suit : " << rank << endl;
+	Rank r = c.rank;
+	Suit s = c.suit;
+	os << "Rank : ";
+	switch (r) {
+	case Two:
+		os << '2';
+		break;
+	case Three:
+		os << '3';
+		break;
+	case Four:
+		os << '4';
+		break;
+	case Five:
+		os << '5';
+		break;
+	case Six:
+		os << '6';
+		break;
+	case Seven:
+		os << '7';
+		break;
+	case Eight:
+		os << '8';
+		break;
+	case Nine:
+		os << '9';
+		break;
+	case Ten:
+		os << '10';
+		break;
+	case Jack:
+		os << 'J';
+		break;
+	case Queen:
+		os << 'Q';
+		break;
+	case King:
+		os << 'K';
+		break;
+	case Ace:
+		os << 'A';
+		break;
+	}
+
+	os << " Suit : ";
+	switch (s) {
+	case SPA:
+		os << 'S';
+		break;
+	case HRT:
+		os << 'H';
+		break;
+	case CLB:
+		os << 'C';
+		break;
+	case DIA:
+		os << 'D';
+		break;
+	}
+
+	return os;
+}
+
+void Card::displayCard()
+{
+   cout << *this << endl;
+}
+
+int main() 
+{
+	Rank r(Ace);
+	cout  << "The card rank is " << r << endl;
+	vector<Card> deck;
+	// add standard 52 cards to deck
+	for (int suit = Suit::SPA; suit <= Suit::DIA; ++suit) {
+    	for ( int rank = Rank::Ace; rank >= Rank::Two; rank--) {
+            deck.push_back(Card(static_cast<Rank>(rank), static_cast<Suit>(suit)));
+        }
+	}
+	for (vector<Card>::iterator i = deck.begin(); i != deck.end(); ++i) i->displayCard();
+	return EXIT_SUCCESS;
 }
--- Card.h
+++ Card.h
@@ -1,29 +1,42 @@
 #ifndef CARD_H
 #define CARD_H
+#include <iostream>
+using namespace std;
 
-enum Rank {Ace=1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King};
-enum Suit { Spades, Hearts, Clubs, Diamonds};
+enum Rank {Two=2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace};
+enum Suit {SPA, HRT, CLB, DIA};
 
 class Card {
 public:
 	// create a “blank” card
 	Card();
 	// constructor to create a card, setting the suit and rank
-	Card(int suit, int rank);
+	Card(Rank rank, Suit suit);
 	// destructor
 	~Card();
 
-	// return the point value of the card. Ace = 1, 2 thru 10, face cards can be 11, 12, 
-	int getValue();
+	// return rank two=2 through to Ace 
+	// return suit SPA, HRT, CLB, DIA
+	int getSuit();
+	int getRank();
+
 	// set an existing card to a particular value
-	void setCard(int suit, int rank);
+	void setCard(Rank rank, Suit suit);
 
 	// display the card
-	void printCard();
+	void displayCard();
+
+	// Overload the "==" and "!=" conparison operators to compare cards
+	//friend bool operator==(const Card& a, const Card& b);
+
+	//friend bool operator!=(const Card& a, const Card& b);
+
+	// overload output stream operator
+	friend ostream& operator<<( ostream&, const Card &);
 
 private:
-	int rank;
-	int suit;
+	Rank rank;
+	Suit suit;
 
 };
 
