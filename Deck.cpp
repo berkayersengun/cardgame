1168babb4dec3d9f6fd9472791431985c6265d68
Berkay Ersengun <berkayersengun@Berkays-MacBook-Pro.local>
2017-11-16 02:09:53 +0000

    a

--- Deck.cpp
+++ Deck.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void createInitialisedCardDeck(int set);
+//void createInitialisedCardDeck(int set);
 
 int main()
 {
@@ -13,10 +13,86 @@ int main()
     cout<<"enter the number of the sets of cards: "<<endl;
     //createEmptyCardDeck();
     cin>>set;
-
-    createInitialisedCardDeck(set);
+    DeckOfCards obj;
+    obj.createInitialisedCardDeck();
+    obj.initialiseCardDeck();
+    //createInitialisedCardDeck(set);
 
 }
+
+
+
+class Card{
+
+public:
+    Card();
+    Card(int cardSuit=1, int cardRank=1);
+     ~Card();
+
+
+private:
+    int Rank;
+    int Suit;
+    int RankNumber;
+    int SuitNumber;
+    int rs;
+};
+
+class DeckOfCards
+{
+	public:
+		DeckOfCards(); // constructor initializes deck
+		void createInitialisedCardDeck(int setNum);
+        void initialiseCardDeck (int setNum, int deck);
+	
+	private:
+		vector< int > deck(52,0); // represents deck of cards and values initialised 0
+	
+}; 
+
+Card::Card (int cardSuit, int cardRank)
+{
+    Rank=cardSuit;
+    Suit=cardRank; 
+    rs=Rank*Suit;
+
+}
+
+DeckOfCards::DeckOfCards()
+{
+    vector<int> newDeck; // create new deck vector
+
+   // populate the deck
+    for (int i = 0; i < Card::SuitNumber; i++)
+    {
+        for (int j = 0; j < Card::RankNumber; j++)
+        {
+            newDeck.push_back(Card(i,j));
+            
+        }
+    } 
+
+	// assign the new deck to the deck vector
+	deck() = newDeck;
+
+
+}
+
+void DeckOfCards::initialiseCardDeck ()
+{
+    for (int i = 0; i < 52; i++)
+	{
+		cout<<deck(i∏);
+
+	
+		
+	}
+
+
+
+}
+
+
 
 
 /*
@@ -24,7 +100,7 @@ void createEmptyCardDeck (void)
 {
     vector<string> duck;
 }
-*/
+
 void createInitialisedCardDeck(int set)
 {
     enum Rank {Ace=1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King};
@@ -51,7 +127,7 @@ void createInitialisedCardDeck(int set)
 
 
 
-/*
+
     vector<int> duck;
     for (int i=0; i<set; i++)
     {
@@ -64,4 +140,7 @@ void createInitialisedCardDeck(int set)
     }
 }   
 
+
+const string Card::faceNames[ RankNumber ] = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+const string Card::suitNames[ SuitNumber ] = {"Hearts", "Clubs", "Diamonds", "Spades"};
 */
